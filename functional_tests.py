680b6045d4f97b0d3f708877dfaf5acc4b40640f
Sharon <Sharon@LP-X220>
2018-12-06 14:51:22 +0800

    Front page HTML generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
         
         #check input box 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         
         #Input an errand to do
         inputbox.send_keys('Buy peacock feathers')
@@ -39,11 +39,12 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do itme did not appear in table"
         )
         
         self.fail('Finish the test!')
 
 if __name__ == "__main__":
-    unittest.main(warning='ignore')
+    unittest.main()
     
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import lists
+from lists import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lists.views.home_page, name='home'),
+    path('', views.home_page, name='home'),
     
 ]
